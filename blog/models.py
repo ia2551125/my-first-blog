2b7a98c1044a76dcd604e986d173c95e94028cf5
Azumi <ia2551125@gmail.com>
2019-04-23 15:30:30 +0900

    20190423_3

--- blog/models.py
+++ blog/models.py
@@ -13,9 +13,9 @@ class Post(models.Model):
     )
 
     def publish(self):
-    self.published_date = timezone.now()
-    self.save()
+        self.published_date = timezone.now()
+        self.save()
 
     def __str__(self):
-    return self.title
+        return self.title
 # Create your models here.
